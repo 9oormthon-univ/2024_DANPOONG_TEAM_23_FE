ff874ec604b86d2a36843ba90f9b37f690da72ad
조성원 <tjddnjsjo@kakao.com>
2024-11-18 01:15:02 +0900

    chore: add react-router-dom for routing and update main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import '~/styles/globals.css'
-import { Root } from '~/routes/root.tsx'
+import { Root } from '~/routes/root'
+
+const router = createBrowserRouter([{ path: '/', element: <Root /> }])
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <Root />
+      <RouterProvider router={router} />
     </StrictMode>,
   )
 } else {
