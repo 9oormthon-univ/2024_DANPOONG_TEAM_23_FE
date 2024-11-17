fbc54f354d94ed7256cc04c0c00a538ff969c190
조성원 <tjddnjsjo@kakao.com>
2024-11-18 01:09:51 +0900

    chore: remove unused files and update main entry point to use new routing structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import '~/styles/globals.css'
+import { Root } from '~/routes/root.tsx'
+
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <Root />
+    </StrictMode>,
+  )
+} else {
+  console.error('Root element not found')
+}
