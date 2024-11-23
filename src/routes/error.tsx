6c940185a52bdf014f6536d025a515aa81dd6ff0
devjaeyoon <devjaeyoon@gmail.com>
2024-11-23 21:19:10 +0900

    feat: add Error page

---
+++ src/routes/error.tsx
@@ -0,0 +1,9 @@
+function Error() {
+  return (
+    <div className="flex h-dvh w-dvw max-w-screen-sm items-center justify-center">
+      <p className="text-xl font-bold text-gray-800">잘못된 접근입니다!</p>
+    </div>
+  )
+}
+
+export { Error }
