487bc415b8ca23f5133833f721eb962b8dba8a14
devjaeyoon <devjaeyoon@gmail.com>
2024-11-23 21:18:48 +0900

    feat: add route protection based on authentication status

--- src/main.tsx
+++ src/main.tsx
@@ -13,15 +13,16 @@ import { Login } from '~/routes/login'
 import { Greeting } from '~/routes/greeting'
 import { Survey } from '~/routes/survey/survey'
 import { HealthDiary } from '~/routes/health-diary'
-import { OnboardingOne } from './routes/onboarding-1'
-import { OnboardingTwo } from './routes/onboarding-2'
-import { OnboardingThree } from './routes/onboarding-3'
-import { MyHealthHistory } from './routes/my-health-history'
-import { MonthlyHealthHistory } from './routes/monthly-health-history'
-import { MyHealthChange } from './routes/my-health-change'
-import { Settings } from './routes/settings'
+import { OnboardingOne } from '~/routes/onboarding-1'
+import { OnboardingTwo } from '~/routes/onboarding-2'
+import { OnboardingThree } from '~/routes/onboarding-3'
+import { MyHealthHistory } from '~/routes/my-health-history'
+import { MonthlyHealthHistory } from '~/routes/monthly-health-history'
+import { MyHealthChange } from '~/routes/my-health-change'
+import { Settings } from '~/routes/settings'
+import { Error } from '~/routes/error'
 
-const router = createBrowserRouter([
+const authorizedRouter = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
@@ -30,23 +31,14 @@ const router = createBrowserRouter([
         path: '/',
         children: [
           { index: true, element: <Root /> },
-          { path: 'landing', element: <Landing /> },
-          { path: 'login', element: <Login /> },
-          { path: 'onboarding-1', element: <OnboardingOne /> },
-          { path: 'onboarding-2', element: <OnboardingTwo /> },
-          { path: 'onboarding-3', element: <OnboardingThree /> },
           { path: 'greeting', element: <Greeting /> },
           { path: 'survey', element: <Survey /> },
           { path: 'my-health-history', element: <MyHealthHistory /> },
           { path: 'monthly-health-history', element: <MonthlyHealthHistory /> },
           { path: 'my-health-change', element: <MyHealthChange /> },
           { path: 'settings', element: <Settings /> },
+          { path: '*', element: <Error /> },
         ],
-      },
-      {
-        path: '/',
-        element: <BaseLayout />,
-        children: [{ path: 'design-system', element: <DesignSystem /> }],
       },
       {
         path: '/',
@@ -57,11 +49,35 @@ const router = createBrowserRouter([
   },
 ])
 
+const publicRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: 'landing', element: <Landing /> },
+      { path: 'login', element: <Login /> },
+      { path: 'onboarding-1', element: <OnboardingOne /> },
+      { path: 'onboarding-2', element: <OnboardingTwo /> },
+      { path: 'onboarding-3', element: <OnboardingThree /> },
+      { path: '*', element: <Login /> },
+    ],
+  },
+  {
+    path: '/',
+    element: <BaseLayout />,
+    children: [{ path: 'design-system', element: <DesignSystem /> }],
+  },
+])
+
 const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <RouterProvider
+        router={
+          localStorage.getItem('accessToken') ? authorizedRouter : publicRouter
+        }
+      />
     </StrictMode>,
   )
 } else {
